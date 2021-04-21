b5ebfd787f8d3e19754f1edb272712efdf4986cd
Luke Edwards <luke.edwards05@gmail.com>
2021-04-21 14:22:07 -0700

    feat(api): setup CORS config

--- workers/api/index.ts
+++ workers/api/index.ts
@@ -1,6 +1,12 @@
 import { Router } from 'worktop';
 import * as Cache from 'worktop/cache';
+import * as CORS from 'worktop/cors';
 
 const API = new Router;
 
+API.prepare = CORS.preflight({
+	origin: '*', // TODO?
+	maxage: 86400, // 1 day
+	credentials: true,
+});
 Cache.listen(API.run);
