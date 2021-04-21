e204e30c5924449fd3d2118204a3b21374711744
Luke Edwards <luke.edwards05@gmail.com>
2021-04-21 14:23:37 -0700

    chore: setup `/auth/*` routes

--- workers/api/index.ts
+++ workers/api/index.ts
@@ -1,6 +1,10 @@
+// Dependencies
 import { Router } from 'worktop';
 import * as Cache from 'worktop/cache';
 import * as CORS from 'worktop/cors';
+
+// Routes
+import * as Auth from './routes/auth';
 
 const API = new Router;
 
@@ -9,4 +13,10 @@ API.prepare = CORS.preflight({
 	maxage: 86400, // 1 day
 	credentials: true,
 });
+
+API.add('POST', '/auth/login', Auth.login);
+API.add('POST', '/auth/register', Auth.register);
+API.add('POST', '/auth/forgot', Auth.forgot);
+API.add('POST', '/auth/reset', Auth.reset);
+
 Cache.listen(API.run);
---
+++ workers/api/routes/auth.ts
@@ -0,0 +1,29 @@
+import type { Handler } from 'worktop';
+
+/**
+ * POST /auth/login
+ */
+export const login: Handler = async (req, res) => {
+	//
+}
+
+/**
+ * POST /auth/register
+ */
+export const register: Handler = async (req, res) => {
+	//
+}
+
+/**
+ * POST /auth/forgot
+ */
+export const forgot: Handler = async (req, res) => {
+	//
+}
+
+/**
+ * POST /auth/reset
+ */
+export const reset: Handler = async (req, res) => {
+	//
+}
