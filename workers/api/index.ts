0187d0692e459eb814d686e244e80855747afe9d
Luke Edwards <luke.edwards05@gmail.com>
2021-04-26 14:10:53 -0700

    feat(api): begin `/spaces` resource

--- workers/api/index.ts
+++ workers/api/index.ts
@@ -5,6 +5,7 @@ import * as CORS from 'worktop/cors';
 
 // Routes
 import * as Auth from './routes/auth';
+import * as Space from './routes/space';
 
 const API = new Router;
 
@@ -19,5 +20,11 @@ API.add('POST', '/auth/register', Auth.register);
 API.add('POST', '/auth/refresh', Auth.refresh);
 API.add('POST', '/auth/forgot', Auth.forgot);
 API.add('POST', '/auth/reset', Auth.reset);
+
+API.add('GET', '/spaces', Space.list);
+API.add('POST', '/spaces', Space.create);
+API.add('GET', '/spaces/:suid', Space.show);
+API.add('PUT', '/spaces/:suid', Space.update);
+API.add('DELETE', '/spaces/:suid', Space.destroy);
 
 Cache.listen(API.run);
---
+++ workers/api/routes/space.ts
@@ -0,0 +1,69 @@
+import { compose } from 'worktop';
+import * as User from 'lib/models/user';
+import * as Space from 'lib/models/space';
+
+import type { SpaceID } from 'lib/models/space';
+
+/**
+ * GET /spaces
+ * @NOTE Requires Authentication
+ */
+export const list = compose(
+	User.authenticate,
+	async function (req, res) {
+		// @ts-ignore - todo(worktop)
+		const user = req.user as User.User;
+	}
+);
+
+/**
+ * POST /spaces
+ * @NOTE Requires Authentication
+ */
+export const create = compose(
+	User.authenticate,
+	async function (req, res) {
+		// @ts-ignore - todo(worktop)
+		const user = req.user as User.User;
+		//
+	}
+);
+
+/**
+ * GET /spaces/:uid
+ * @NOTE Requires Authentication
+ */
+export const show = compose(
+	User.authenticate,
+	function (req, res) {
+		// @ts-ignore - todo(worktop)
+	}
+);
+
+/**
+ * PUT /spaces/:uid
+ * @NOTE Requires Authentication
+ */
+export const update = compose(
+	User.authenticate,
+	async function (req, res) {
+		// @ts-ignore - todo(worktop)
+		const user = req.user as User.User;
+		// @ts-ignore - todo(worktop)
+		const space = req.space as Space.Space;
+	}
+);
+
+/**
+ * DELETE /spaces/:uid
+ * @NOTE Requires Authentication
+ */
+export const destroy = compose(
+	User.authenticate,
+	async function (req, res) {
+		// @ts-ignore - todo(worktop)
+		const user = req.user as User.User;
+		// @ts-ignore - todo(worktop)
+		const space = req.space as Space.Space;
+	}
+);
