fd54fdd765c66e1ee420cfbd77b083dca3b891ec
Luke Edwards <luke.edwards05@gmail.com>
2021-04-28 12:22:57 -0700

    feat(utils): import `database.paginate` util

--- lib/utils/database.ts
+++ lib/utils/database.ts
@@ -1,5 +1,6 @@
 import * as DB from 'worktop/kv';
 import type { KeyID } from 'lib/utils/keys';
+import type { KV } from 'worktop/kv';
 
 // TODO(worktop): add expiration settings
 export function write<T>(key: KeyID, value: T): Promise<boolean> {
@@ -12,4 +13,35 @@ export function read<T>(key: KeyID): Promise<T|false> {
 
 export function remove(key: KeyID): Promise<boolean> {
 	return DB.remove(DATABASE, key);
+}
+
+interface PaginateData<T> {
+	done: boolean;
+	keys: T;
+}
+
+interface PaginateOptions {
+	limit?: number;
+	raw?: boolean;
+}
+
+// NOTE: already in next worktop
+type KeyInfo = KV.KeyList['keys'];
+export function paginate(prefix: string, options: PaginateOptions & { raw: true }): AsyncGenerator<PaginateData<KeyInfo>>;
+export function paginate(prefix: string, options?: PaginateOptions & { raw: false }): AsyncGenerator<PaginateData<string[]>>;
+export async function * paginate(prefix: string, options: PaginateOptions = {}): AsyncGenerator<PaginateData<KeyInfo|string[]>> {
+	let { limit, raw } = options;
+	let cursor: string | undefined;
+
+	while (true) {
+		let results = await DATABASE.list({ prefix, limit, cursor });
+		cursor = results.cursor;
+
+		yield {
+			done: results.list_complete,
+			keys: raw ? results.keys : results.keys.map(x => x.name),
+		};
+
+		if (results.list_complete) return;
+	}
 }
