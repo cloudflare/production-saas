c24272422ec772a75a2e6d922096e65dbcf5d74f
Luke Edwards <luke.edwards05@gmail.com>
2021-05-05 13:58:22 -0700

    chore: add fake `zoneid` value

--- cfw.config.js
+++ cfw.config.js
@@ -5,8 +5,9 @@ const ENV = process.env;
  * @type {import('cfw').Config}
  */
 module.exports = {
-	profile: 'personal',
 	entry: 'index.ts',
+	zoneid: '<<zoneid>>',
+	profile: 'personal',
 	globals: {
 		DATABASE: 'KV:802e0f5c830c45d09fcae3f506579341',
 		JWT_SECRET: `SECRET:${ENV.JWT_SECRET}`,
