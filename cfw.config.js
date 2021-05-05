c2f34dba1cef32942abd166fd6e61b3af181e264
Luke Edwards <luke.edwards05@gmail.com>
2021-05-05 16:23:26 -0700

    chore: attach doman/zone value

--- cfw.config.js
+++ cfw.config.js
@@ -6,8 +6,7 @@ const ENV = process.env;
  */
 module.exports = {
 	entry: 'index.ts',
-	zoneid: '<<zoneid>>',
-	profile: 'personal',
+	profile: 'workers.demo',
 	globals: {
 		DATABASE: 'KV:802e0f5c830c45d09fcae3f506579341',
 		JWT_SECRET: `SECRET:${ENV.JWT_SECRET}`,
